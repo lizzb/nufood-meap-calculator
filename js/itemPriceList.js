af06870c22c86cb078532147756e9b6eadfd67ef
lizzb <lizz@lizz-natalia.local>
2013-11-08 12:30:42 -0600

    deleted old item list and fx to make it
    also some extranous comments /variables from makeproductlist

--- js/itemPriceList.js
+++ js/itemPriceList.js
@@ -221,64 +221,6 @@ var products = [
 	
 ];
 
-/*
-var items = {
-	"Soup of the day": "0.00",
-
-	"Chicken Caesar Salad": "5.59",
-	"Tomato + Mozzerella Baguette": "4.99",
-
-	"Celery + Carrots (Crudites)": "2.69",
-	"Edamame": "2.95",
-
-	"Sushi - Veggie": "0.00",
-	"Sushi - Shrimp": "0.00",
-	"Sushi - California w/ Masago": "0.00",
-	"Sushi - California": "0.00",
-	
-	"Nerd Rope": "1.19",
-	"Odwalla Juice Smoothie": "3.49",
-	"Sweettart Gummies": "2.75",
-
-	"Tostido's Lime Tortilla Chips": "4.29",
-	"Spicy Cheeto Fries": "1.49",
-
-	"Lean Cuisine": "4.99",
-	"Amy's Pesto Tortellini Bowl": "5.49",
-	"Taipei Stir Fry Chinese Takeout Boxes": "4.99",
-	"Bagel Bites Mini Pizzas": "0.00",
-	"Naanwich": "0.00",
-
-	"Simply Asia Soup/Noodles": "4.99",
-
-	"Cup O Ramen": "0.00",
-	"Top Ramen Package": "0.49",
-
-	"PB&J on Wheat": "2.99",
-	"Brownie": "1.99",
-	"Danish": "2.25",
-	"Cookie": "1.75",
-	"Machine Coffee Drink 16 oz": "3.00",
-
-	"Dorito's": "0.00",
-	"Soda - 12 pack cans": "0.00",
-	"Soda - Individual bottle": "0.00",
-	
-	"Chocolate Candy Bar": "0.00",
-	"Clif Bar": "0.00",
-	
-	"Candy Bar": "1.19",
-	"M&Ms": "1.19",
-	"Laffy Taffy": "0.25",
-	"Mini Reese's Cups": "0.25",
-	"Gum": "0.00",
-	
-	"Wheat Thins": "0.00",
-	"Box of cereal": "0.00",
-	"Mini bowl of cereal": "0.00",
-	
-};*/
-
 	// section: "Frozen Meals",
 	// section: "Baked Goods",
 	// section: "Candy, Chips, & Soda",
@@ -288,39 +230,19 @@ var items = {
 	// section: "Cold Drinks",
 	// section: "Frozen Treats",
 
-/* // uses items
-function makeProductList() {
-
-	var productlisthtml = "";
-	var i = 0;
-
-	//$.each( items, function( item, price ) {
-	$.each( products, function( name, price, section ) {
-	  	productlisthtml +=
-	  	"<li class='product purchase btn btn-info item' id='"+i+"'>" +
-	  	"<span class='title'>" + name + "</span>" +
-	  	"<span class='price'> $" + price + "</span> </li>";
-	  i++;
-	});
-
-	document.getElementById("products").innerHTML = "<ul>" + productlisthtml + "</ul>";
-}
-*/
 
 function makeProductList() {
 
 	var productlisthtml = "";
-	var i = 0;
 
-	$.each( products, function( index ) { //, value
+	$.each( products, function( index ) {
 		// name = products[i][0]
 		// price = products[i][1]
 		// section = products[i][2]
 	  	productlisthtml +=
-	  	"<li class='product purchase btn btn-info item' id='"+ index+ "'>" + //+i+"'>" +
+	  	"<li class='product purchase btn btn-info item' id='"+ index+ "'>" +
 	  	"<span class='title'>" + products[index].name + "</span>" +
 	  	"<span class='price'> $" + products[index].price  + "</span> </li>";
-	  //i++;
 	});
 
 	document.getElementById("products").innerHTML = "<ul>" + productlisthtml + "</ul>";
